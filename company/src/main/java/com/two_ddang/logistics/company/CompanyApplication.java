c6f7f244231e28bd1b51d7faf4e956b1394b4eb2
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-09-10 00:25:13 +0900

    feat: company Service core 멀티 모듈 적용

--- company/src/main/java/com/two_ddang/logistics/company/CompanyApplication.java
+++ company/src/main/java/com/two_ddang/logistics/company/CompanyApplication.java
@@ -3,7 +3,12 @@ package com.two_ddang.logistics.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.two_ddang.logistics.company",
+				"com.two_ddang.logistics.core"
+		}
+)
 public class CompanyApplication {
 
 	public static void main(String[] args) {

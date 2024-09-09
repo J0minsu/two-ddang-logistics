d5524dab47797d6547562ced83dc44129d9d0afe
Minsu Jo <tok0419@naver.com>
2024-09-09 10:39:22 +0900

    SETTING: hub project init

---
+++ hub/src/test/java/com/two_ddang/logistics/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.two_ddang.logistics.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

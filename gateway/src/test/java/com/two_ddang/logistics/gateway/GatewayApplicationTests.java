1e2584c5d2395c594ee4852d3bdbb5f0d967e457
asd42270 <lyb991113@gmail.com>
2024-09-09 12:46:05 +0900

    SETTING: gateway project init

---
+++ gateway/src/test/java/com/two_ddang/logistics/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.two_ddang.logistics.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

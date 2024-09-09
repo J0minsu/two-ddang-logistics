d76f0566b62f7f304fd1a3c66ac80c92cdddcf2b
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-09-10 00:35:23 +0900

    feat: company ResponseDTO, ErrorCode 추가

---
+++ company/src/main/java/com/two_ddang/logistics/company/common/exceptions/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.two_ddang.logistics.company.common.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    // COMMON
+    NOT_FOUND(HttpStatus.NOT_FOUND, "경로가 올바르지 않습니다");
+
+    private final HttpStatus httpStatus;
+
+    private final String message;
+
+    ErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}

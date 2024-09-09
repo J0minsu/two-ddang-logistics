8fcce81d5a7ccbda7bec5fa7b48f5672576848d9
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-09-09 10:51:19 +0900

    SETTING: order project init

---
+++ order/src/test/java/com/sparta/b2b/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.b2b.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4cf59bb9f345d2da02f29623a16361510a8f1a3f
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-09-09 22:00:06 +0900

    feat: order Service core 멀티 모듈 적용

--- order/src/main/java/com/two_ddang/logistics/order/OrderApplication.java
+++ order/src/main/java/com/two_ddang/logistics/order/OrderApplication.java
@@ -2,9 +2,16 @@ package com.two_ddang.logistics.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
-public class OrderApplication {
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.two_ddang.logistics.order",
+				"com.two_ddang.logistics.core"
+		}
+)
+@EnableJpaAuditing
+public class OrderApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);

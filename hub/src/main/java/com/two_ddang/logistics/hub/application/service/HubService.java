6938ba4b39570245a44ea5a9f50593733e68f0e1
Minsu Jo <tok0419@naver.com>
2024-09-10 22:32:36 +0900

    Feat : #10 and swagger

---
+++ hub/src/main/java/com/two_ddang/logistics/hub/application/service/HubService.java
@@ -0,0 +1,18 @@
+package com.two_ddang.logistics.hub.application.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class HubService {
+
+
+
+}

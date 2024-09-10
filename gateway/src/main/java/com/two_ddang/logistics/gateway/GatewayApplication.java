9ad033443951edea10d4f988c87782baa69e76e4
asd42270 <lyb991113@gmail.com>
2024-09-10 10:10:29 +0900

    feat: Gateway를 구현한다. - application.yml: 각 서버로 라우팅 설정을 한다. - GatewayApplication: 공통 모듈 적용을 위한 설정 - JwtAuthenticationFilter: Jwt를 검증하고 PassPort로 바꾸어 전달한다. - PassPort: 내부 인증 객체를 만든다.

--- gateway/src/main/java/com/two_ddang/logistics/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/two_ddang/logistics/gateway/GatewayApplication.java
@@ -3,7 +3,12 @@ package com.two_ddang.logistics.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+		"com.two-ddang.logistics.gateway",
+		"com.two-ddang.logistics.core"
+		}
+)
 public class GatewayApplication {
 
 	public static void main(String[] args) {

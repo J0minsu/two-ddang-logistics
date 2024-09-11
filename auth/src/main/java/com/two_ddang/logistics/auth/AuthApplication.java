7499fd063115deeee003c8054d23d2c9472bdb10
asd42270 <lyb991113@gmail.com>
2024-09-11 19:05:57 +0900

    feat: re-commit - application.yml: auth, server, gateway 재설정 - AuthApplication: 글자 깨짐 현상으로 수정

--- auth/src/main/java/com/two_ddang/logistics/auth/AuthApplication.java
+++ auth/src/main/java/com/two_ddang/logistics/auth/AuthApplication.java
@@ -3,7 +3,12 @@ package com.two_ddang.logistics.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.two-ddang.logistics.auth",
+				"com.two-ddang.logistics.core"
+		}
+)
 public class AuthApplication {
 
 	public static void main(String[] args) {
---
+++ auth/src/main/java/com/two_ddang/logistics/auth/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.two_ddang.logistics.auth.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+}

8dc873c2ccea68c097c09158110981fdc489a29e
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-09-19 01:34:20 +0900

    #45 feat: product swagger 추가

--- company/src/main/java/com/two_ddang/logistics/company/presentation/controller/product/ProductController.java
+++ company/src/main/java/com/two_ddang/logistics/company/presentation/controller/product/ProductController.java
@@ -8,6 +8,8 @@ import com.two_ddang.logistics.company.presentation.dtos.product.UpdateProductIn
 import com.two_ddang.logistics.core.util.CurrentPassport;
 import com.two_ddang.logistics.core.util.Passport;
 import com.two_ddang.logistics.core.util.ResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,16 +18,19 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "상품 API")
 public class ProductController {
 
     private final ProductService productService;
 
     @PostMapping
+    @Operation(summary = "상품 생성", description = "상품 생성 API")
     public ResponseEntity<ResponseDTO<CreateProductResponse>> createProduct(
             @RequestBody CreateProductRequest createProductRequest,
             @CurrentPassport Passport passport)
@@ -35,14 +40,20 @@ public class ProductController {
 
 
     @GetMapping
-    public ResponseEntity<ResponseDTO<Page<ProductResponse>>> getProducts(@PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-                                                                          @RequestParam("keyword") String keyword) {
-
+    @Operation(summary = "상품 리스트 조회", description = "상품 생성 API")
+    public ResponseEntity<ResponseDTO<Page<ProductResponse>>> getProducts(
+            @PageableDefault(page = 0,
+                    size = 10,
+                    sort = "createdAt",
+                    direction = Sort.Direction.DESC) Pageable pageable,
+            @RequestParam("keyword") String keyword)
+    {
         return ResponseEntity.ok(ResponseDTO.okWithData(productService.getProducts(pageable, keyword)));
     }
 
 
     @GetMapping("/{productId}")
+    @Operation(summary = "상품 단건 조회", description = "상품 단건 조회 API")
     public ResponseEntity<ResponseDTO<ProductDetailResponse>> getProduct(@PathVariable UUID productId) {
         return ResponseEntity.ok(ResponseDTO.okWithData(productService.getProduct(productId)));
     }
@@ -50,6 +61,7 @@ public class ProductController {
 
 
     @PatchMapping("/{productId}")
+    @Operation(summary = "상품 정보 수정", description = "상품 정보 수정 API")
     public ResponseEntity<ResponseDTO<UpdateProductInfoResponse>> updateProductInfo(
             @PathVariable UUID productId,
             @RequestBody UpdateProductInfoRequest updateProductInfoRequest,
@@ -61,7 +73,8 @@ public class ProductController {
 
 
     @PostMapping("/{productId}/rollback")
-    public ResponseEntity<?> rollbackStock(@PathVariable UUID productId,
+    @Operation(summary = "상품 재고 롤백", description = "상품 재고 롤백 API")
+    public ResponseEntity<ResponseDTO<Void>> rollbackStock(@PathVariable UUID productId,
                                            @RequestBody RollbackStockRequest rollbackStockRequest) {
         productService.rollbackStock(productId, rollbackStockRequest);
         return ResponseEntity.ok(ResponseDTO.ok());
@@ -69,6 +82,7 @@ public class ProductController {
 
 
     @DeleteMapping("/{productId}")
+    @Operation(summary = "상품 삭제", description = "상품 삭제 API")
     public ResponseEntity<ResponseDTO<Void>> deleteProduct(@PathVariable UUID productId, @CurrentPassport Passport passport) {
         productService.deleteProduct(productId, passport);
         return ResponseEntity.ok(ResponseDTO.ok());
@@ -76,7 +90,8 @@ public class ProductController {
 
 
     @GetMapping("/companies/{companyId}")
-    public ResponseEntity<?> getCompanyProducts(@PathVariable UUID companyId) {
+    @Operation(summary = "업체 상품 리스트 조회", description = "업체 상품 리스트 조회 API")
+    public ResponseEntity<ResponseDTO<List<CompanyProductResponse>>> getCompanyProducts(@PathVariable UUID companyId) {
         return ResponseEntity.ok(ResponseDTO.okWithData(productService.getCompanyProducts(companyId)));
     }
 }
--- company/src/main/java/com/two_ddang/logistics/company/presentation/dtos/product/UpdateProductInfoRequest.java
+++ company/src/main/java/com/two_ddang/logistics/company/presentation/dtos/product/UpdateProductInfoRequest.java
@@ -1,5 +1,6 @@
 package com.two_ddang.logistics.company.presentation.dtos.product;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,12 +8,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Schema(title = "상품 정보 수정 요청 DTO", description = "상품 정보 수정을 위한 데이터 전송 객체")
 public class UpdateProductInfoRequest {
 
+    @Schema(title = "상품명", example = "Updated Product Name")
     private String productName;
+
+    @Schema(title = "상품 설명", example = "Updated product description.")
     private String description;
+
+    @Schema(title = "가격", example = "15000")
     private Long price;
+
+    @Schema(title = "품절 여부", example = "false", description = "상품이 품절되었는지 여부")
     private Boolean isSoldOut;
-
-
 }

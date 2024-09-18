50e01c1c103f5e7625479cacb9fb3a0dd1f07710
asd42270 <lyb991113@gmail.com>
2024-09-18 21:21:05 +0900

    feat: 위치 정보를 통해 위도 경도 호출 로직 구현 - Google의 Geocoding 사용

---
+++ ai/src/main/java/com/two_ddang/logistics/ai/application/dto/LatLngRequestDto.java
@@ -0,0 +1,8 @@
+package com.two_ddang.logistics.ai.application.dto;
+
+public record LatLngRequestDto(
+        Double latitude,
+        Double longitude
+) {
+
+}
--- ai/src/main/java/com/two_ddang/logistics/ai/application/service/GeminiService.java
+++ ai/src/main/java/com/two_ddang/logistics/ai/application/service/GeminiService.java
@@ -1,16 +1,22 @@
 package com.two_ddang.logistics.ai.application.service;
 
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import com.two_ddang.logistics.ai.application.dto.GeminiReadResponseDto;
+import com.two_ddang.logistics.ai.application.dto.LatLngRequestDto;
 import com.two_ddang.logistics.ai.application.service.feign.delivery.DeliveryService;
 import com.two_ddang.logistics.ai.application.service.feign.delivery.dto.DeliveryRes;
-import com.two_ddang.logistics.core.entity.AiType;
 import com.two_ddang.logistics.ai.domain.model.Gemini;
-import com.two_ddang.logistics.ai.infrastructure.exception.AINotFoundException;
 import com.two_ddang.logistics.ai.domain.repository.GeminiRepository;
-import com.two_ddang.logistics.ai.application.dto.GeminiReadResponseDto;
+import com.two_ddang.logistics.ai.infrastructure.exception.AINotFoundException;
+import com.two_ddang.logistics.core.entity.AiType;
 import com.two_ddang.logistics.core.entity.DeliveryStatus;
 import com.two_ddang.logistics.core.entity.DriverAgentType;
-import lombok.RequiredArgsConstructor;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Async;
@@ -18,17 +24,31 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
 public class GeminiService {
 
     private final GeminiRepository geminiRepository;
     private final VertexAiGeminiChatModel vertexAiGeminiChatModel;
     private final DeliveryService deliveryService;
+
+    private final String geoCodingApiKey;
+
+    public GeminiService(GeminiRepository geminiRepository,
+                         VertexAiGeminiChatModel vertexAiGeminiChatModel,
+                         DeliveryService deliveryService,
+                         @Value("${google.maps.api.key}")String geoCodingApiKey) {
+        this.geminiRepository = geminiRepository;
+        this.vertexAiGeminiChatModel = vertexAiGeminiChatModel;
+        this.deliveryService = deliveryService;
+        this.geoCodingApiKey = geoCodingApiKey;
+    }
+
 
     public String chatToGeminiAndSave(String prompt, Long userId, AiType aiType) {
 
@@ -70,7 +90,42 @@ public class GeminiService {
     @Scheduled(cron = "0 0 6 * * *") //매일 오전 6시
     @Async
     protected void transitAgentDeliveryInfo() {
-        List<DeliveryRes> responses = deliveryService.getTransitAddressAndSlackId(DriverAgentType.TRANSIT, DeliveryStatus.BEFORE_TRANSIT);
+        List<DeliveryRes> responses = deliveryService.getTransitAddressAndSlackId(DriverAgentType.TRANSIT,
+                DeliveryStatus.BEFORE_TRANSIT);
+
+        List<LatLngRequestDto> requestToGemini = new ArrayList<>();
+
+        responses.forEach(response -> {
+            String address = response.address();
+
+            GeoApiContext context = new GeoApiContext.Builder()
+                    .apiKey(geoCodingApiKey)
+                    .build();
+
+            try {
+                //요청을 동기적으로 실행 -> 비동기 식으로 바꿔줘야 함.
+                GeocodingResult[] results = GeocodingApi.geocode(context, address).await();
+                if (results.length > 0) {
+                    LatLng location = results[0].geometry.location;
+                    requestToGemini.add(new LatLngRequestDto(location.lat, location.lng));
+
+
+
+                }
+
+
+
+
+            } catch (IOException | InterruptedException | ApiException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+
+
+
+
 
     }
 }

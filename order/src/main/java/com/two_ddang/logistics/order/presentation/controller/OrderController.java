b6f015badf1f2f5d569043debbbc401b411bd378
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-09-19 01:40:00 +0900

    #45 feat: order swagger 추가

--- order/src/main/java/com/two_ddang/logistics/order/presentation/controller/OrderController.java
+++ order/src/main/java/com/two_ddang/logistics/order/presentation/controller/OrderController.java
@@ -9,6 +9,8 @@ import com.two_ddang.logistics.order.application.dtos.response.*;
 import com.two_ddang.logistics.order.application.service.OrderService;
 import com.two_ddang.logistics.order.presentation.dtos.CreateOrderRequest;
 import com.two_ddang.logistics.order.presentation.dtos.UpdateOrderStatusRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -24,17 +26,20 @@ import java.util.UUID;
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "주문 API")
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
+    @Operation(summary = "주문 생성", description = "주문 생성 API")
     public ResponseEntity<ResponseDTO<CreateOrderResponse>> createOrder(@RequestBody CreateOrderRequest createOrderRequestDto) {
         return ResponseEntity.ok(ResponseDTO.okWithData(orderService.createOrder(createOrderRequestDto)));
     }
 
 
     @GetMapping
+    @Operation(summary = "주문 리스트 조회", description = "주문 리스트 조회 API")
     public ResponseEntity<ResponseDTO<Page<OrderResponse>>> getOrders(
             @PageableDefault(page = 0,
                     size = 10,
@@ -49,6 +54,7 @@ public class OrderController {
 
 
     @GetMapping("/{orderId}")
+    @Operation(summary = "주문 단건 조회", description = "주문 단건 조회 API")
     public ResponseEntity<ResponseDTO<OrderDetailResponse>> getOrder(@PathVariable("orderId") UUID orderId,
                                                                      @CurrentPassport Passport passport) {
         return ResponseEntity.ok(ResponseDTO.okWithData(orderService.getOrder(orderId, passport)));
@@ -56,6 +62,7 @@ public class OrderController {
 
 
     @PatchMapping("/{orderId}")
+    @Operation(summary = "주문 상태 변경", description = "주문 상태 변경 API")
     public ResponseEntity<ResponseDTO<UpdateOrderStatusResponse>> updateOrderStatus(
             @PathVariable("orderId") UUID orderId,
             @RequestBody UpdateOrderStatusRequest updateOrderStatusRequestDto,
@@ -66,6 +73,7 @@ public class OrderController {
 
 
     @PostMapping("/{orderId}/cancel")
+    @Operation(summary = "주문 취소", description = "주문 취소 API")
     public ResponseEntity<ResponseDTO<CancelOrderResponse>> cancelOrder(@PathVariable("orderId") UUID orderId,
                                                                         @CurrentPassport Passport passport)
     {
@@ -74,6 +82,7 @@ public class OrderController {
 
 
     @DeleteMapping("/{orderId}")
+    @Operation(summary = "주문 삭제", description = "주문 삭제 API")
     public ResponseEntity<ResponseDTO<Void>> deleteOrder(@PathVariable("orderId") UUID orderId,
                                                          @CurrentPassport Passport passport) {
         orderService.deleteOrder(orderId, passport);

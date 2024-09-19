a5e84ac1f1316b0b575a253768e899462c0523f4
asd42270 <lyb991113@gmail.com>
2024-09-19 12:57:45 +0900

    feat: transitjson 세부 사항 입력

--- core/src/main/java/com/two_ddang/logistics/core/entity/TransitJson.java
+++ core/src/main/java/com/two_ddang/logistics/core/entity/TransitJson.java
@@ -1,6 +1,13 @@
 package com.two_ddang.logistics.core.entity;
 
+import java.util.UUID;
+
 public record TransitJson(
+        Integer sequence,
+        UUID departmentHubId,
+        UUID arriveHubId,
+        Integer totalEstimateDistance
+
 ) {
 
 }

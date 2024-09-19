0bb83fc1337f43c6f0fa875ebfb0e777255ab16f
asd42270 <lyb991113@gmail.com>
2024-09-19 20:12:09 +0900

    security 의존성 제거, CustomUser 제거

--- ai/src/main/java/com/two_ddang/logistics/ai/infrastructure/config/CustomUser.java
+++ ai/src/main/java/com/two_ddang/logistics/ai/infrastructure/config/CustomUser.java
@@ -1,54 +1,54 @@
-package com.two_ddang.logistics.ai.infrastructure.config;
-
-import com.two_ddang.logistics.core.util.Passport;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Arrays;
-import java.util.Collection;
-
-public class CustomUser implements UserDetails {
-
-    private final Passport passport;
-
-    public CustomUser(Passport passport) {
-        this.passport = passport;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority(passport.getUserType().getUserType()));
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
-
-}
+//package com.two_ddang.logistics.ai.infrastructure.config;
+//
+//import com.two_ddang.logistics.core.util.Passport;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Arrays;
+//import java.util.Collection;
+//
+//public class CustomUser implements UserDetails {
+//
+//    private final Passport passport;
+//
+//    public CustomUser(Passport passport) {
+//        this.passport = passport;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Arrays.asList(new SimpleGrantedAuthority(passport.getUserType().getUserType()));
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return UserDetails.super.isAccountNonExpired();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return UserDetails.super.isAccountNonLocked();
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return UserDetails.super.isCredentialsNonExpired();
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return UserDetails.super.isEnabled();
+//    }
+//
+//}
